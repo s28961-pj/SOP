be2c3a88f13f39c81c4f78fb5b076732b0332319
s27440-pj <s27440@pjwstk.edu.pl>
2023-05-05 16:32:03 +0200

    wskazniki

---
+++ s27440-KrystynaTokarska/zajecia4/zadWsk.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/* typedef int (*f)(int, int); */
+
+int max(int, int);
+int min(int, int);
+int sum(int, int);
+
+
+int main(){
+	int (*f[3])(int, int) = {max, min, sum};
+	
+	int functionID, elementCount, v;
+	printf("Podaj ID funkcji 0-max, 1-min, 2-sum: \n");
+	scanf("%d", &functionID);
+	printf("Podaj liczbe argumentow, ktora chcesz pwrowadzic: \n");
+	scanf("%d", &elementCount);
+	printf("Wprowadz elementy: \n");
+	scanf("%d", &v);
+
+	int i = 1;
+	for (i; i < elementCount; i++){
+		int x;
+		scanf("%d", &x);
+		v = f[functionID](v, x);
+	}
+
+	printf("wynik: %d\n", v);
+	return 0;
+}
+
+int max(int a, int b){
+	return (a < b ? b : a);
+}
+
+int min(int a, int b){
+	return (a < b ? a : b);
+}
+
+int sum(int a, int b){
+	return (a + b);
+}
+
