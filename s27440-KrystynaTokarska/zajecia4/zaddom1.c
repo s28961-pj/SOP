77af674edbc024cf96aafa0042ac35805c76a08a
s27440-pj <s27440@pjwstk.edu.pl>
2023-05-13 18:20:45 +0200

    dodano zadania z implementacji list

---
+++ s27440-KrystynaTokarska/zajecia4/zaddom1.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct wezel{
+	int dane;
+	struct wezel* nastepny;
+};
+
+void drukuj_liste(struct wezel* poczatek){
+	printf("Elementy listy: \n");
+	while (poczatek != NULL){
+		printf("%d ", poczatek->dane);
+		poczatek = poczatek->nastepny;
+	}
+	printf("\n");
+}
+
+void dodaj_przod(struct wezel **poczatek, int wartosc){
+	struct wezel *nowy_wezel = malloc(sizeof(struct wezel));
+	if (nowy_wezel == NULL){
+		printf("Nie udalo sie zarezerwowac pamieci na nowy element\n");
+	}
+	nowy_wezel->dane = wartosc;
+
+	nowy_wezel->nastepny = *poczatek;
+	*poczatek = nowy_wezel;
+}
+
+void dodaj_tyl(struct wezel **poczatek, int wartosc){
+	struct wezel *nowy_wezel = malloc(sizeof(struct wezel));
+	if (nowy_wezel == NULL){
+		printf("Nie udalo zarezerwowac pamieci na nowy element\n");
+	}
+	nowy_wezel->dane = wartosc;
+
+	struct wezel *ostatni = *poczatek;
+	while (ostatni->nastepny != NULL){
+		ostatni = ostatni->nastepny;
+	}
+	ostatni->nastepny = nowy_wezel;
+}
+
+void usun_przod(struct wezel **poczatek){
+	if (*poczatek == NULL){
+		printf("Lista jest pusta.\n");
+		return;
+	}
+	struct wezel *tmp = *poczatek;
+	*poczatek = (*poczatek)->nastepny;
+	free(tmp);
+}
+
+void usun_tyl(struct wezel **poczatek){
+	if (*poczatek == NULL){
+		printf("Lista jest pusta.\n");
+		return;
+	}
+	if((*poczatek)->nastepny == NULL){
+		free(*poczatek);
+		*poczatek = NULL;
+		return;
+	}
+	struct wezel *tmp = *poczatek;
+	while(tmp->nastepny->nastepny != NULL){
+	tmp = tmp->nastepny;
+	}
+	free(tmp->nastepny);
+	tmp->nastepny = NULL;
+}
+
+int main(){
+	struct wezel *lista = NULL;
+	dodaj_przod(&lista,2);
+	dodaj_tyl(&lista,5);
+	dodaj_przod(&lista,10);
+	dodaj_tyl(&lista,1);
+	drukuj_liste(lista);
+	printf("Usuwam jeden element z przodu i jeden z tylu\n");
+	usun_przod(&lista);
+	usun_tyl(&lista);
+	drukuj_liste(lista);
+	return 0;
+}
